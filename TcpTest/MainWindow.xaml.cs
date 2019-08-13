bc4fed47d1980c279f21cc35c6dd011d8a9ffd3e
npe <notpoorelephant@gmail.com>
2019-08-13 15:35:59 +0900

    Add project files.

---
+++ TcpTest/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TcpTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Socketクライアント
+        TcpClient tClient = new TcpClient();
+
+        public MainWindow()
+        {
+            Debug.WriteLine("Form1" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            InitializeComponent();
+
+            //接続OKイベント
+            tClient.OnConnected += new TcpClient.ConnectedEventHandler(tClient_OnConnected);
+            //接続断イベント
+            tClient.OnDisconnected += new TcpClient.DisconnectedEventHandler(tClient_OnDisconnected);
+            //データ受信イベント
+            tClient.OnReceiveData += new TcpClient.ReceiveEventHandler(tClient_OnReceiveData);
+        }
+
+        /** 接続断イベント **/
+        void tClient_OnDisconnected(object sender, EventArgs e)
+        {
+            Debug.WriteLine("tClient_OnDisconnected" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            if (Dispatcher.CheckAccess())
+                Dispatcher.Invoke(new DisconnectedDelegate(Disconnected), new object[] { sender, e });
+        }
+        delegate void DisconnectedDelegate(object sender, EventArgs e);
+        private void Disconnected(object sender, EventArgs e)
+        {
+            //接続断処理
+            Debug.WriteLine("Disconnected" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+
+        /** 接続OKイベント **/
+        void tClient_OnConnected(EventArgs e)
+        {
+            //接続OK処理
+            Debug.WriteLine("tClient_OnConnected" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        /** 接続ボタンを押して接続処理 **/
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("btn_Connect_Click" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            try
+            {
+                //接続先ホスト名
+                string host = "127.0.0.1";
+                //接続先ポート
+                int port = int.Parse(portnum.Text);
+                //接続処理
+                // Connect to the remote endpoint.
+                tClient.Connect(host, port);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /** データ受信イベント **/
+        void tClient_OnReceiveData(object sender, string e)
+        {
+            Debug.WriteLine("tClient_OnReceiveData" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            //別スレッドからくるのでInvokeを使用
+            if (Dispatcher.CheckAccess())
+                Dispatcher.Invoke(new ReceiveDelegate(ReceiveData), new object[] { sender, e });
+        }
+        delegate void ReceiveDelegate(object sender, string e);
+        //データ受信処理
+        private void ReceiveData(object sender, string e)
+        {
+            Debug.WriteLine("ReceiveData:" + e + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //送信
+            tClient.Send(textBox.Text);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Form1_FormClosing" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            if (!tClient.IsClosed)
+                tClient.Close();
+
+        }
+
+        private void ServerStart_Click(object sender, RoutedEventArgs e)
+        {
+            Program.StartServer(portnum.Text);
+        }
+
+        private void SSend_Click(object sender, RoutedEventArgs e)
+        {
+
+            Program.Instance.Send(stextBox.Text);
+        }
+    }
+}
---
+++ TcpTest/Progam.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TcpTest
+{
+    public class Program
+    {
+        static public Program Instance { get; set; }
+        public static void StartServer(string portnum)
+        {
+            try
+            {
+                Console.WriteLine("Main ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+                Program prog = new Program(portnum);
+                prog.init();
+                Instance = prog;
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+
+        private static ManualResetEvent SocketEvent = new ManualResetEvent(false);
+
+        private IPEndPoint ipEndPoint;
+
+        private Socket sock;
+
+        private Thread tMain;
+        //送受信文字列エンコード
+        private Encoding enc = Encoding.UTF8;
+
+        Program(String port)
+        {
+            Console.WriteLine("Program ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            //IPAddress myIP = Dns.GetHostByName(Dns.GetHostName()).AddressList[0];
+            IPAddress myIP = IPAddress.Parse("127.0.0.1");
+            ipEndPoint = new IPEndPoint(myIP, Int32.Parse(port));
+        }
+
+        void init()
+        {
+            Console.WriteLine("init ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(ipEndPoint);
+            sock.Listen(10);
+            Console.WriteLine("サーバー起動中・・・");
+            tMain = new Thread(new ThreadStart(Round));
+            tMain.Start();
+        }
+
+        void Round()
+        {
+            Console.WriteLine("Round ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            while (true)
+            {
+                SocketEvent.Reset();
+                sock.BeginAccept(new AsyncCallback(OnConnectRequest), sock);
+                SocketEvent.WaitOne();
+            }
+        }
+
+        //★
+        Socket Gl_socket;
+        void OnConnectRequest(IAsyncResult ar)
+        {
+            Console.WriteLine("OnConnectRequest ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            SocketEvent.Set();
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+            //★
+            Gl_socket = handler;
+            Console.WriteLine(handler.RemoteEndPoint.ToString() + " joined");
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BUFFER_SIZE, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        void ReadCallback(IAsyncResult ar)
+        {
+            Console.WriteLine("ReadCallback ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+            int ReadSize = handler.EndReceive(ar);
+            if (ReadSize < 1)
+            {
+                Console.WriteLine(handler.RemoteEndPoint.ToString() + " disconnected");
+                return;
+            }
+            byte[] bb = new byte[ReadSize];
+            Array.Copy(state.buffer, bb, ReadSize);
+            string msg = System.Text.Encoding.UTF8.GetString(bb);
+            Console.WriteLine(msg);
+            handler.BeginSend(bb, 0, bb.Length, 0, new AsyncCallback(WriteCallback), state);
+        }
+
+        void WriteCallback(IAsyncResult ar)
+        {
+            Console.WriteLine("WriteCallback ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+            handler.EndSend(ar);
+            Console.WriteLine("送信完了");
+            handler.BeginReceive(state.buffer, 0, StateObject.BUFFER_SIZE, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        void disConnect()
+        {
+            Console.WriteLine("disConnect ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            sock.Close();
+        }
+        /// <summary>
+        /// ★メッセージを送信する
+        /// </summary>
+        /// <param name="str"></param>
+        public void Send(string str)
+        {
+            Debug.WriteLine("Send" + " ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+
+            //if (!IsClosed)
+            //{
+                //文字列をBYTE配列に変換
+                byte[] sendBytes = enc.GetBytes(str + "\r\n");
+            //lock (syncLock)
+            //{
+            //送信
+            //mySocket.Send(sendBytes);
+            //sock.Send(sendBytes);
+            Gl_socket.Send(sendBytes);
+            //sock.BeginSend(sendBytes,);
+            //}
+            //}
+        }
+
+    }
+
+    class StateObject
+    {
+        public Socket workSocket { get; set; }
+        public const int BUFFER_SIZE = 1024;
+        internal byte[] buffer = new byte[BUFFER_SIZE];
+    }
+}
